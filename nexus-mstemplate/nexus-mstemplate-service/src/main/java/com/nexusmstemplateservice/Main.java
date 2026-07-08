d70934befa304b43d7d7f367b4ceae563d4ccdc9
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-09 00:06:34 +0800

    服务发现 Nacos 模板

---
+++ nexus-mstemplate/nexus-mstemplate-service/src/main/java/com/nexusmstemplateservice/Main.java
@@ -0,0 +1,14 @@
+package com.nexusmstemplateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ quick_start.md
@@ -0,0 +1,3 @@
+### 1. nexus-mstemplate
+
+集成 Nacos，微服务添加到服务发现 Nacos 模板
