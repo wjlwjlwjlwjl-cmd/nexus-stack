f871a2ff97b8062f01042dd0a5f0745eed3d1d2b
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-09 23:32:28 +0800

    网关处统一异常处理

--- nexus-gateway/src/main/java/com/nexus/nexusgateway/GatewayApplication.java
+++ nexus-gateway/src/main/java/com/nexus/nexusgateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.nexus.bitegateway;
+package com.nexus.nexusgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
