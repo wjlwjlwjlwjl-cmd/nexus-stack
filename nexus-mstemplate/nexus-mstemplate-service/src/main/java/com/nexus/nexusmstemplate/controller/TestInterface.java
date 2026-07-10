dab04df5c634eaed26d40fb1b1d4fbc570e5a5dc
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-10 17:29:50 +0800

    封装线程池完成测试

--- nexus-mstemplate/nexus-mstemplate-service/src/main/java/com/nexus/nexusmstemplate/controller/TestInterface.java
+++ nexus-mstemplate/nexus-mstemplate-service/src/main/java/com/nexus/nexusmstemplate/controller/TestInterface.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/test/interface")
 @Slf4j
-public class Test {
+public class TestInterface {
     @GetMapping("/info")
     public void info(){
         log.info("接口调用成功");
