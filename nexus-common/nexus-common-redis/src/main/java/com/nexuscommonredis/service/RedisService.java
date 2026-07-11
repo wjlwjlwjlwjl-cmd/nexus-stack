ce84f602df0b53869d043839a3a130b1b38b1e45
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-11 09:24:04 +0800

    Redis 封装（未完成）

--- nexus-common/nexus-common-redis/src/main/java/com/nexuscommonredis/service/RedisService.java
+++ nexus-common/nexus-common-redis/src/main/java/com/nexuscommonredis/service/RedisService.java
@@ -1,10 +1,13 @@
 package com.nexuscommonredis.service;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -156,7 +159,7 @@ public class RedisService {
      */
     public <T> T getCacheObject(final String key, Class<T> clazz){
         ValueOperations<String, T> operation = redisTemplate.opsForValue();
-        T t = (T)operation.get(key);
+        Object t = operation.get(key);
         if(t == null){  //解析失败
             return null;
         }
@@ -173,16 +176,253 @@ public class RedisService {
      */
     public <T> T getCacheObject(final String key, TypeReference<T> reference){
         Object obj = redisTemplate.opsForValue().get(key);
-        T t = (T)obj;
         if(obj == null) return null;
-        return JsonUtil.string2Object(JsonUtil.object2String(t), reference);
+        return JsonUtil.string2Object(JsonUtil.object2String(obj), reference);
     }
 
     //************** */
     // list 操作
     //************** */
     
+    /**
+     * 缓存 list 数据
+     * 
+     * @param <T>       list 元素类型
+     * @param key       list 键值
+     * @param dataList  list 对象
+     * @return          添加元素后 list 长度
+     */
     public <T> long setCacheList(final String key, final List<T> dataList){
+        ListOperations operations = redisTemplate.opsForList();
+        Long count = operations.rightPushAll(key, dataList);
+        return count == null ? 0 : count;
+    }
 
+    /**
+     * 从左侧插入单个对象（头插）
+     * 
+     * @param <T>   添加元素类型
+     * @param key   list 键值
+     * @param value 添加元素
+     */
+    public <T> void leftPushForList(final String key, final T value){
+        redisTemplate.opsForList().leftPush(key, value);
+    }
+
+    /**
+     * 从右侧插入单个对象（尾插）
+     * 
+     * @param <T>   添加元素类型
+     * @param key   list 键值
+     * @param value 添加元素
+     */
+    public <T> void rightPushForList(final String key, final T value){
+        redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    /**
+     * 删除最左侧元素（头删）
+     * 
+     * @param key 目标键值
+     */
+    public void leftPopForList(final String key){
+        redisTemplate.opsForList().leftPop(key);
+    }
+
+    /**
+     * 删除最右侧元素（尾删）
+     * 
+     * @param key 目标键值
+     */
+    public void rightPopForList(final String key){
+        redisTemplate.opsForList().rightPop(key);
+    }
+
+    /**
+     * 从列表中移除第一个匹配的元素
+     * 
+     * @param <T>       移除元素的类型
+     * @param key       list 对应的键值
+     * @param value     移除元素
+     */
+    public <T> void removeForList(final String key, T value){
+        redisTemplate.opsForList().remove(key, 1, value);
+    }
+
+    /**
+     * 从列表中删除所有匹配的元素
+     * 
+     * @param <T>       移除元素的类型
+     * @param key       list 对应的键值
+     * @param value     移除的元素
+     */
+    public <T> void removeAllForList(final String key, T value){
+        redisTemplate.opsForList().remove(key, 0, value);
+    }
+
+    /**
+     * 删除一个 list 中的所有元素（不是删除 list 本身）
+     * 
+     * @param key list 对应的 key
+     */
+    public void removeForAllList(final String key){
+        redisTemplate.opsForList().trim(key, -1, 0); 
+    }
+
+    /**
+     * 修改 list 中指定元素
+     * 
+     * @param <T>   元素类型
+     * @param key   list 键值
+     * @param index value
+     * @param value
+     */
+    public <T> void setElementAtIndex(final String key, int index, T value){
+        redisTemplate.opsForList().set(key, index, value);
+    }
+
+    /**
+     * 获取整个列表，并指定列表元素类型（简单元素）
+     * 
+     * @param <T>       列表元素类型
+     * @param key       list 键值
+     * @param clazz     列表元素类对象
+     * @return          获取到的列表
+     */
+    public <T> List<T> getCacheList(final String key, Class<T> clazz){
+        List list = redisTemplate.opsForList().range(key, 0, -1); // Redis 对于列表区间的指定策略是左闭右闭
+        return JsonUtil.string2List(JsonUtil.object2String(list), clazz);
+    }
+
+    /**
+     * 获取整个列表，并指定列表元素类型（复杂类型）
+     * 
+     * @param <T>           列表元素类型
+     * @param key           list 键值
+     * @param reference     复杂对象类型
+     * @return              获取到的列表
+     */
+    public <T> List<T> getCacheList(final String key, TypeReference<List<T>> reference){
+        List list = redisTemplate.opsForList().range(key, 0, -1);
+        return JsonUtil.string2Object(JsonUtil.object2String(list), reference);
+    }
+
+    /**
+     * 获得[start, end]的子列表
+     * 
+     * @param <T>       列表元素类型
+     * @param key       list 键值 
+     * @param start     subList 起始位置
+     * @param end       subList 结束位置
+     * @param clazz     列表元素类对象
+     * @return          获取到的列表
+     */
+    public <T> List<T> getCacheListByRange(final String key, int start, int end, Class<T> clazz){
+        List list = redisTemplate.opsForList().range(key, start, end);
+        return JsonUtil.string2List(JsonUtil.object2String(list), clazz);
+    }
+
+    /**
+     * 获得[start, end]的子列表（复杂类型）
+     * 
+     * @param <T>       列表元素类型
+     * @param key       list 键值
+     * @param start     subList 起始位置
+     * @param end       subList 结束位置
+     * @param reference 列表元素类对象
+     * @return          获取到的列表
+     */
+    public <T> List<T> getCacheListByRange(final String key, int start, int end, TypeReference<List<T>> reference){
+        List<T> list = redisTemplate.opsForList().range(key, start, end);
+        return list;
+    }
+
+    /**
+     * 获取列表长度
+     * 
+     * @param key   list 键值
+     * @return      list 长度
+     */
+    public Long getCacheListSize(final String key){
+        Long ret = redisTemplate.opsForList().size(key);
+        return ret == null ? 0L : ret;
+    }
+
+    //************ */
+    // Set 类型
+    //************ */
+
+    /**
+     * 向 Set 中添加一个或多个元素
+     * 
+     * @param key   Set 键值
+     * @param objs  要插入的一个或多个元素
+     */
+    public void addMember(final String key, Object... objs){
+        redisTemplate.opsForSet().add(key, objs);
+    }
+
+    /**
+     * 从 Set 中移除一个或多个元素
+     * 
+     * @param key   Set 键值
+     * @param objs  要删除的一个或多个元素
+     */
+    public void deleteMember(final String key, Object... objs){
+        redisTemplate.opsForSet().remove(key, objs);
+    }
+
+    /**
+     * 获取集合中的所有元素（复杂类型）
+     * 
+     * @param <T>           Set 中单个元素类型
+     * @param key           Set 键值
+     * @param reference     复杂类型
+     * @return              查询得到的 Set
+     */
+    public <T> Set<T> getCacheSet(final String key, TypeReference<Set<T>> reference){
+        Set set = redisTemplate.opsForSet().members(key);
+        return JsonUtil.string2Object(JsonUtil.object2String(set), reference);
+    }
+
+    //******************* */
+    // Zset 类型
+    //******************* */
+
+    /**
+     * 添加单个元素到 ZSet 中
+     * 
+     * @param key       Zset 键值
+     * @param obj       要添加的元素
+     * @param seqNo     seqNo，元素分数
+     */
+    public void addMemberZSet(final String key, Object obj, double seqNo){
+        redisTemplate.opsForZSet().add(key, obj, seqNo);
+    }
+
+    /**
+     * 删除ZSet中的某个元素
+     * 
+     * @param key   ZSet 键值
+     * @param obj   要移除的对象
+     */
+    public void delMemberZSet(final String key, Object obj){
+        redisTemplate.opsForZSet().remove(key, obj);
+    }
+
+    /**
+     * 删除分数在 [minScore, maxScore] 区间的 ZSet 中的元素
+     * 
+     * @param key           Zset 键值
+     * @param startScore    起始分数
+     * @param endScore      结束分数
+     */
+    public void removeZSetByScore(final String key, int startScore, int endScore){
+        redisTemplate.opsForZSet().removeRangeByScore(key, startScore, endScore);
+    }
+
+    public <T> Set<T> getCacheZSet(final String key, TypeReference<LinkedHashSet<T>> reference){
+        Set set = redisTemplate.opsForZSet().range(key, 0, -1);
+        return JsonUtil.string2Object(JsonUtil.object2String(set), reference);
     }
 }
