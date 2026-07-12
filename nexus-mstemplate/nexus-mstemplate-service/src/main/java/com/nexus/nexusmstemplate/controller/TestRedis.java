b4df243d70dad0e13ca532e9f3cc67dc04148013
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-12 15:17:21 +0800

    引入 RabbitMQ 消息队列

--- nexus-mstemplate/nexus-mstemplate-service/src/main/java/com/nexus/nexusmstemplate/controller/TestRedis.java
+++ nexus-mstemplate/nexus-mstemplate-service/src/main/java/com/nexus/nexusmstemplate/controller/TestRedis.java
@@ -1,8 +1,5 @@
 package com.nexus.nexusmstemplate.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nexus.nexuscommonredis.service.RedisService;
-import com.nexus.nexusmstemplate.domain.User;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,16 +24,6 @@ public class TestRedis {
 
     @PostMapping("/kv/set")
     public void set(String key, String val){
-        User user1 = new User(val, 10, val);
-        User user2 = new User(val, 10, val);
-        User user3 = new User(val, 10, val);
-        List<User> list = new ArrayList<>();
-
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-
-        redisService.setCacheObjectIfAbsent(key, list);
     }
 
     @GetMapping("/kv/get")
