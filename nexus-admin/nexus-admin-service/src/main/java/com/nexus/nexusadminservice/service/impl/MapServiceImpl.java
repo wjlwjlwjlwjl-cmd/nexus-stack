1fff61d94f9eb611348cbff9c6beb923865a4917
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-13 13:53:48 +0800

    引入本地缓存解决 BigKey 问题

--- nexus-admin/nexus-admin-service/src/main/java/com/nexus/nexusadminservice/service/impl/MapServiceImpl.java
+++ nexus-admin/nexus-admin-service/src/main/java/com/nexus/nexusadminservice/service/impl/MapServiceImpl.java
@@ -2,17 +2,19 @@ package com.nexus.nexusadminservice.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.github.benmanes.caffeine.cache.Cache;
 import com.nexus.nexusadminaqi.constants.MapConstants;
 import com.nexus.nexusadminservice.dao.MapDao;
 import com.nexus.nexusadminservice.domain.dto.SysRegionDTO;
 import com.nexus.nexusadminservice.domain.entity.SysRegion;
 import com.nexus.nexusadminservice.service.IMapService;
-
+import com.nexus.nexuscommoncache.util.CacheUtil;
 import com.nexus.nexuscommoncore.utils.BeanCopyUtil;
 import com.nexus.nexuscommonredis.service.RedisService;
 
@@ -26,12 +28,18 @@ public class MapServiceImpl implements IMapService{
     MapDao dao;
 
     @Autowired
+    CacheUtil cacheUtil;
+
+    @Autowired
     RedisService redisService;
+
+    @Autowired
+    Cache<String, Object> cache;
 
     @Override
     public List<SysRegionDTO> get_list() {
         List<SysRegionDTO> regions;
-        regions = redisService.getCacheObject(MapConstants.CACHE_MAP_CITY_KEY, new TypeReference<List<SysRegionDTO>>(){});
+        regions = cacheUtil.getCache(MapConstants.CACHE_MAP_CITY_KEY, new TypeReference<List<SysRegionDTO>>(){}, cache, redisService);
         if(regions != null){
             return regions;
         }
@@ -46,8 +54,8 @@ public class MapServiceImpl implements IMapService{
             }
         }
 
-        redisService.setCacheObject(MapConstants.CACHE_MAP_CITY_KEY, regions);
-        log.warn("向Redis中存储城市列表");
+        log.warn("向两级缓存中存储");
+        cacheUtil.setL2Cache(MapConstants.CACHE_MAP_CITY_KEY, regions, cache, redisService, 1000, TimeUnit.SECONDS);
         return regions;
     }
 }
